482cd4158bb4c9b0242d2bfecf2d14c586cdf2bd
Pavan Kusuma <newtonpavan33@gmail.com>
2026-02-18 16:19:14 +0530

    Feb 18 2

--- app/api/v2/reservations/[...ids]/route.js
+++ app/api/v2/reservations/[...ids]/route.js
@@ -17,7 +17,7 @@ export async function GET(request,{params}) {
             if(params.ids[1] == 'U0'){
                 try {
                     // lets update the query to add user table as well to get user details
-                    var query = 'SELECT r.*, p.name, p.description, p.size, p.tags, p.media, p.prm, p.std, p.isActive, u.name as dealer, u.mobile, u.mapTo from reservations r LEFT JOIN products1 p ON r.design = p.design LEFT JOIN user u ON r.userId = u.id ORDER BY r.createdOn DESC LIMIT 20 OFFSET '+params.ids[3];
+                    var query = 'SELECT r.*, p.name, p.productId, p.description, p.size, p.tags, p.media, p.prm, p.std, p.isActive, u.name as dealer, u.mobile, u.mapTo from reservations r LEFT JOIN products1 p ON r.design = p.design LEFT JOIN user u ON r.userId = u.id ORDER BY r.createdOn DESC LIMIT 20 OFFSET '+params.ids[3];
                     var queryCount = 'SELECT count(*) as count from reservations r LEFT JOIN products1 p ON r.design = p.design LEFT JOIN user u ON r.userId = u.id';
 
                     // if status is provided then filter by status as well
@@ -28,7 +28,7 @@ export async function GET(request,{params}) {
                         //     query = 'SELECT r.*, p.*, u.name as dealer, u.mobile, u.mapTo from reservations r LEFT JOIN products1 p ON r.design = p.design LEFT JOIN user u ON r.userId = u.id WHERE r.expiryDate > r.createdOn ORDER BY r.createdOn DESC LIMIT 20 OFFSET '+params.ids[3];
                         // }
                         // else
-                        query = 'SELECT r.*, p.name, p.description, p.size, p.tags, p.media, p.prm, p.std, p.isActive, u.name as dealer, u.mobile, u.mapTo from reservations r LEFT JOIN products1 p ON r.design = p.design LEFT JOIN user u ON r.userId = u.id WHERE r.status="'+params.ids[2]+'" ORDER BY r.createdOn DESC LIMIT 20 OFFSET '+params.ids[3];
+                        query = 'SELECT r.*, p.name, p.productId, p.description, p.size, p.tags, p.media, p.prm, p.std, p.isActive, u.name as dealer, u.mobile, u.mapTo from reservations r LEFT JOIN products1 p ON r.design = p.design LEFT JOIN user u ON r.userId = u.id WHERE r.status="'+params.ids[2]+'" ORDER BY r.createdOn DESC LIMIT 20 OFFSET '+params.ids[3];
                         queryCount = 'SELECT count(*) as count from reservations r LEFT JOIN products1 p ON r.design = p.design LEFT JOIN user u ON r.userId = u.id WHERE r.status="'+params.ids[2]+'"';
                     }
 
@@ -50,7 +50,7 @@ export async function GET(request,{params}) {
             // get the list of reservations by userId
             else if(params.ids[1] == 'U1'){
                 try {
-                    const [rows, fields] = await connection.execute('SELECT r.*, p.name, p.description, p.size, p.tags, p.media, p.prm, p.std, p.isActive from reservations r LEFT JOIN products1 p ON r.design = p.design WHERE r.userId="'+params.ids[2]+'" ORDER BY r.createdOn DESC LIMIT 20 OFFSET '+params.ids[3]);
+                    const [rows, fields] = await connection.execute('SELECT r.*, p.name, p.productId, p.description, p.size, p.tags, p.media, p.prm, p.std, p.isActive from reservations r LEFT JOIN products1 p ON r.design = p.design WHERE r.userId="'+params.ids[2]+'" ORDER BY r.createdOn DESC LIMIT 20 OFFSET '+params.ids[3]);
                     const [countRows, countFields] = await connection.execute('SELECT count(*) as count from reservations r LEFT JOIN products1 p ON r.design = p.design WHERE r.userId="'+params.ids[2]+'"');
                     connection.release();
 
@@ -66,7 +66,7 @@ export async function GET(request,{params}) {
             // get the list of reservations by design
             else if(params.ids[1] == 'U2'){
                 try {
-                    const [rows, fields] = await connection.execute('SELECT r.*, p.name, p.description, p.size, p.tags, p.media, p.prm, p.std, p.isActive from reservations r LEFT JOIN products1 p ON r.design = p.design WHERE r.design="'+params.ids[2]+'" ORDER BY r.createdOn DESC');
+                    const [rows, fields] = await connection.execute('SELECT r.*, p.name, p.productId, p.description, p.size, p.tags, p.media, p.prm, p.std, p.isActive from reservations r LEFT JOIN products1 p ON r.design = p.design WHERE r.design="'+params.ids[2]+'" ORDER BY r.createdOn DESC');
                     connection.release();
 
                     if(rows.length > 0)
